d1c7639b7061ea0cc2d32e63bd1b6732bacac85e
Alex Flores <domain@me.com>
2016-10-02 23:08:59 -0400

    transform sudo code

--- app.js
+++ app.js
@@ -2,29 +2,40 @@
 
 // psuedocode for triggering a tweet when the plant needs water
 //
-var five = johnny five library
-var rpi  = raspi-io library
-var twitter = twitter api
-
-// create a variable to hold an object that represents GPIO pin 5
-var input = rpi.pin(5)
+var five = require("johnny-five")
+var Raspi = require("raspi-io")
+var board = new five.Board({ io: new Raspi() })
+var Twitter = require('twitter')
+var twitterClient = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
 
 // voltage threshold that says 'this plant needs water'
 var threshold = process.env.THRESHOLD
+
 // whether a user has been notified that a  plant needs water
 var notified = false
 
-five.onBoardReady(function() {
+board.on("ready", function() {
 
-        while(true) {
+  // Create a new generic sensor instance for
+  // a sensor connected to an analog (ADC) pin
+  var sensor = new five.Sensor("A0")
 
-                if (input.voltage < threshold) {
-                        notified = false
-                } else {
-                        if (!notified) {
-                                twitter.tweet('user', 'water me')
-                                notified = true
-                        }
-                }
-        }
+  sensor.on("change", function(value) {
+    if (value > threshold) {
+      notified = false
+    } else {
+      if (!notified) {
+        twitterClient.post('statuses/update', {status: 'PLANT DEATH IMMINENT!'}, function(error, tweet, response) {
+          if (!error) {
+            console.log(tweet)
+          }
+        })
+      }
+    }
+  })
 })
